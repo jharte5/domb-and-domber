b2b76949d10c5ac770d693287ba78bf31843393b
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-09 15:18:29 -0400

    initial commit

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-
+function appendListItem (element) {
+    const uList = document.querySelector('ul');
+    uList.appendChild(element);
+}
+function appendDiv (element) {
+    const dDiv = document.querySelector('div');
+    document.querySelector = dDiv.appendChild(element);
+    div.innerText = 'id=lorem';
+}
+function takeInText ( )
