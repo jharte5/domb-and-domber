ab7baa181c7899fdb51f5196e4ebdfa3268dda5a
jharte5 <jonathan.harte@codeimmersives.com>
2019-10-10 12:38:14 -0400

    updated class

--- main.js
+++ main.js
@@ -3,14 +3,25 @@ function appendListItem (element) {
     uList.appendChild(element);
 }
 function appendDiv (element) {
-    const dDiv = document.querySelector('div');
+    const dDiv = document.querySelector('#lorem');
     dDiv.appendChild(element);
-    div.innerText = 'id=lorem';
 }
 function takeInText (str) {
     const newLi = document.createElement('li');
-    newLi.innerText = 'innerText';
+    newLi.innerText = str;
 }
-function takeInImg (element, str) {
+function takeInImg (url, image) {
+    image.src = url;
+    const newImage = document.createElement('img');
+    const newUrl = 'url'; newImage
+    takeInImg(newUrl, newImage);
+}
+function sClass (elementClone, elementOriginal) {
+    elementClone.className = elementOriginal.className
+}
+const thing1 = document.querySelector('#thing-1');
+const thing2 = document.querySelector('#thing-2');
+const thingC = document.querySelector('#thing-c');
 
-}
+sClass(thing1, thing2);
+sClass(thing1, thingC);
